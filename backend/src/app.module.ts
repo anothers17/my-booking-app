612efb6f8f62fcf7fb030f32a20270a9cc1a102c
C <sulhee.intern@myorder.ai+git@gitlab.com>
2025-09-14 12:21:37 +0700

    fix module backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,16 +6,15 @@ import { MessengerModule } from './messenger/messenger.module';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
 import { GoogleSheetsModule } from './google-sheets/google-sheets.module';
-import { HealthModule } from './health/health.module';
-import { BookingnestModule } from './generate/bookingnest/bookingnest.module';
-import { BookingModule } from './booking/booking.module';
-import { AuthModule } from './auth/auth.module';
-import { MessengerModule } from './messenger/messenger.module';
-import { GoogleSheetsModule } from './google-sheets/google-sheets.module';
-import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [BookingModule, MessengerModule, AdminModule, AuthModule, GoogleSheetsModule, HealthModule, BookingnestModule],
+  imports: [
+    BookingModule,
+    MessengerModule,
+    AdminModule,
+    AuthModule,
+    GoogleSheetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
